f9d3884e63f8d63cac8107bb4b2fb74fb6d0b688
Vishnu <codemaker2015@gmail.com>
2022-10-30 15:01:57 +0530

    initial commit

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public void MenuScene() {
+        SceneManager.LoadScene("MenuScene");
+    }
+    
+    public void Bars() {
+        SceneManager.LoadScene("Bars");
+    }
+
+    public void Basic() {
+        SceneManager.LoadScene("Basic");
+    }
+
+    public void Blinking() {
+        SceneManager.LoadScene("Blinking");
+    }
+
+    public void CircleHalf1() {
+        SceneManager.LoadScene("CircleHalf1");
+    }
+    
+    public void CircleHalf2() {
+        SceneManager.LoadScene("CircleHalf2");
+    }
+
+    public void Cog() {
+        SceneManager.LoadScene("Cog");
+    }
+
+    public void CircleOpen() {
+        SceneManager.LoadScene("CircleOpen");
+    }
+
+    public void CircleQuarter() {
+        SceneManager.LoadScene("CircleQuarter");
+    }
+
+    public void Folding() {
+        SceneManager.LoadScene("Folding");
+    }
+
+    public void PicAxe() {
+        SceneManager.LoadScene("PicAxe");
+    }
+
+    public void Pulsing() {
+        SceneManager.LoadScene("Pulsing");
+    }
+
+    public void Spinner() {
+        SceneManager.LoadScene("Spinner");
+    }
+
+    public void Square() {
+        SceneManager.LoadScene("Square");
+    }
+
+    public void Stopwatch() {
+        SceneManager.LoadScene("Stopwatch");
+    }
+
+    public void Sword() {
+        SceneManager.LoadScene("Sword");
+    }
+
+    public void Triangle() {
+        SceneManager.LoadScene("Triangle");
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Unity3D Loader Animations
+
+Demo project contains some loader animations such as bars, spinner, stopwatch etc...
+
+![demo](Demos/demo.gif)
